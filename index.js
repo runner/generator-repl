f65ae310a02c3d1d22f5f3e78273c7acc8a4b044
Stanislav Kalashnik <s.kalashnik@infomir.com>
2019-05-10 17:33:18 +0300

    fix options extension

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ function generator ( config, options ) {
             }
         }
     }, config || {});
-    options = Object.assign(generator.options, options || {});
+    options = Object.assign({}, generator.options, options || {});
 
     tasks[options.prefix + 'config' + options.suffix] = function () {
         log.inspect(config, log);
